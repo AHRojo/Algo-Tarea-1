a1aba4f46737030b07d3244f135c799b81b317df
MeatBoyUSM <andres.huerta.14@sansano.usm.cl>
2016-10-08 20:00:20 -0300

    Update Tarea1.c

--- Tarea1.c
+++ Tarea1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+int construirMatriz(){
+    int i, j, Matriz[999][999];
+    printf("hola\n");
+    for(i = 0; i < 999; i++){
+            Matriz[i][j] = fgets("%s", sizeof(char*999), stdin);
+        }
+    }
+    for(i = 0; i < 999; i++){
+        printf("%c", Matriz[i][0]);
+    }
+    return Matriz;
+}
+
 int main(){
-  
-  return 0;
+    printf("hola\n");
+    construirMatriz();
+
+    return 0;
 }
