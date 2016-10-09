82ae0fd2de88da29e503336706201b905c7fc429
MeatBoyUSM <andres.huerta.14@sansano.usm.cl>
2016-10-09 16:42:36 -0300

    Update cola.c

--- cola.c
+++ cola.c
@@ -3,7 +3,7 @@
 #include "cola.h"
 
 
-tCola * newCola (int capacidad){
+tCola * newCola(int capacidad){
     tCola *cola;
     cola = (tCola *)malloc(sizeof(tCola));
     cola ->contenido = (tCoordenada *)malloc(sizeof(tCoordenada) * capacidad);
@@ -12,6 +12,10 @@ tCola * newCola (int capacidad){
     cola ->primero = 0;
     cola ->final = -1;
     return cola;
+}
+
+int SCola(tCola *Q){
+    return Q->size;
 }
 
 tCoordenada * dequeue(tCola * cola){
@@ -47,10 +51,28 @@ int enqueue(tCola * cola, tCoordenada * elemento){
 }
 
 void destroy(tCola * cola){
-    for(int i = 0; i < cola->maximoElementos;i++){
-        free(cola->contenido[i]);
-    }
     free(cola->contenido);
     free(cola);
     return;
 }
+/*
+int main(){
+    tCoordenada * co1 = (tCoordenada *)malloc(sizeof(tCoordenada));
+    tCoordenada * co2 = (tCoordenada *)malloc(sizeof(tCoordenada));
+    co1->fila=1;
+    co1->columna=10;
+    co2->fila=35;
+    co2->columna=2;
+    tCola * cola = newCola(3);
+    enqueue(cola, co1);
+    enqueue(cola, co2);
+
+    tCoordenada * co3;
+    co3 = dequeue(cola);
+    printf("fila: %d columna: %d\n",co3->fila,co3->columna);
+    co3 = dequeue(cola);
+    printf("fila: %d columna: %d\n",co3->fila,co3->columna);
+    return 0;
+}
+*/
+
