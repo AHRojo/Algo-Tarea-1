3b70f2c837406dd1331316df9bda523fc0802687
FelipitoX <felipe.montero.cl@gmail.com>
2016-10-10 23:30:46 -0300

    Add files via upload

--- ArbolEj2.c
+++ ArbolEj2.c
@@ -1,11 +1,35 @@
+typedef struct guarda{
+  int nivel;
+  int ;
+}num;
 
-typedef struct treeNodeMiddle {
-  tNode* padre;
-  tNode* hijo;
-  int Valor;
-} tNode;
 
-typedef struct treeLeaf {
-  tNode* padre;
-  int valor;
-}tLeaf
+
+
+
+int * busqueda_num(char* arreglo,int* numeros,int peso,int consultas){
+  int nivel,i,mayor_num,menor_niv,j;
+  nivel=-1;
+  while (i<consultas){
+    i=0;
+    if (numero[i]>peso){
+      if (i==0){
+        return 0;
+      }
+      if (menor_niv>nivel){
+        menor_niv=nivel;
+        mayor_num=i;
+      }
+    }
+    if (strcmp(arreglo[j],"(")) == 0){
+      nivel++;
+      j++;
+      i++;
+    }
+    else{
+      nivel--;
+      j++;
+    }
+  }
+  return i;
+}
