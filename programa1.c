0c16cdc5eb8cb4779641821d7b512e7a58820bac
MeatBoyUSM <andres.huerta.14@sansano.usm.cl>
2016-10-16 22:11:48 -0300

    Update programa1.c
    Se agregaron los comentarios

--- programa1.c
+++ programa1.c
@@ -2,8 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+    La variable datos guarda en las posiciones pares, la marca de la variable y en los impares
+    el area que tiene.
+*/
+
 char Matriz[1000][1000];
-int counter = 0, indice = 0, datos[1000];
+int counter = 0, indice = 0, datos[1500];
+
+/*
+    La funcion recursivo, toma la posición entregada por el terminal (dicha posición siempre debe ser W),
+    corresponde a un recorrido profundo, cada vez que encuentre una posición que contenga una W, se volvera a invocar
+    y sumara 1 al total del area calculada.
+    Ademas la función "marca" la posicion con un valor de la tabla ascii, en caso de que se repitan los valores ya escogidos.
+*/
 
 int recursivo(int fila, int columna){
     Matriz[fila][columna] = 'A'+counter;
@@ -35,6 +47,17 @@ int recursivo(int fila, int columna){
     return Area;
 }
 
+/*
+    consultarArea recibe como parametro la palabra que recibio un valor distinto a W o L
+    y lo descompone de forma que queden 2 valores, fila y columna. Luego revisara la Matriz
+    en caso de que dicha posicion posea una W, significa que la posicion aun no ha sido revisada
+    asi que llamara a la funcion recursiva para calcular el area.
+
+    En caso de que haya una letra distinta a W o L, quiere decir que la posicion ya fue revisada o
+    es una direccion no valida respectivamente, si ya fue revisada entonces buscara dentro de un arreglo
+    el valor del codigo ascii y guardara el valor siguiente del arreglo.
+*/
+
 void consultarArea(char *loc){
     int fila, columna, area;
     if(loc[0] == '\n') return;
@@ -53,10 +76,15 @@ void consultarArea(char *loc){
             area = recursivo(fila - 1, columna - 1);
         }
         else{
-            for(area = 0; area < indice; area += 2){
-                if(datos[area] == Matriz[fila - 1][columna - 1]){
-                    area = datos[area + 1];
-                    break;
+            if(Matriz[fila -1][columna - 1] == 'L'){
+                printf("Direccion invalida.\n");
+            }
+            else{
+                for(area = 0; area < indice; area += 2){
+                    if(datos[area] == Matriz[fila - 1][columna - 1]){
+                        area = datos[area + 1];
+                        break;
+                    }
                 }
             }
         }
@@ -73,6 +101,14 @@ void consultarArea(char *loc){
     return;
 }
 
+/*
+    La funcion main se encarga principalmente de copiar los valores que se ingresan por terminal
+    correspondientes a la información de la matriz y luego llama a la funcion consultarArea,
+    cuando se termina de trabajar con dicha matriz se resetean sus valores con la funcion memset
+
+    Finalmente printea los valores del area guardadas en la variable datos.
+*/
+
 int main(){
     int i, n, j;
     char Palabra[1000];
@@ -83,8 +119,8 @@ int main(){
         for(j = 0; j < 999; j++){
             fgets(Palabra, sizeof(Palabra), stdin);
             if ((Palabra[0] != 'W') && (Palabra[0] != 'L')){
-                Matriz[i][0] = '\0';
                 consultarArea(Palabra);
+                memset(Matriz, 0, sizeof(Matriz[1000][1000])*1000*1000);
                 break;
             }
             strcpy(Matriz[j], Palabra);
